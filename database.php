53c7c51be7acafdf1837c2a59b78ed5bbea7c1db
rasonyudha4 <rasonyudha921@gmail.com>
2024-10-15 03:52:28 +0700

    Completing the backend

--- database.php
+++ database.php
@@ -3,12 +3,35 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=Softeng", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+$conn = mysqli_connect($servername, $username, $password, 'softeng');
+if(!$conn) {
+  echo "Database connection failed";
+}
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+  $newuser = $_POST['username'];
+  $newPassword = $_POST['userPassword'];
+  $newEmail = $_POST['userEmail'];
+
+  $registerQuery = mysqli_prepare($conn, "INSERT INTO users (name, email, password) VALUES (?, ?, ?");
+  $registerQuery->bind_param("sss", $newuser, $newEmail, $newPassword);
+  $registerQuery->execute();
+  mysqli_close($conn);
+  header('Location: Home.html');
+
+} elseif ($_SERVER["REQUEST_METHOD"] === "GET") {
+  $user = $_GET['username'];
+  $password = $_GET['password'];
+
+  if($user and $password !== '') {
+    $loginQuery = "SELECT name, password FROM users WHERE name = ? AND password = ?";
+    $exist = mysqli_prepare($conn, $loginQuery);
+    $exist = bind_param("ss", $user, $password);
+    $exist->execute();
+    if($exist->num_rows > 0) {
+      mysqli_close($conn);
+      header('Location: Home.html');
+    }
+  }
 }
 ?>
