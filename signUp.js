9a12d67246f53fcb7fcc1a50729cb7f535088e8e
mico-m31 <mico.martin@student.president.ac.id>
2024-10-14 12:04:45 +0700

    adding signUp and login logic

---
+++ signUp.js
@@ -0,0 +1,9 @@
+document.getElementById('').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('userEmail').value;
+    const password = document.getElementById('userPassword').value;
+
+    if(username )
+})
